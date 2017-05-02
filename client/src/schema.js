dcb1831e82f3f22f09856750dd412b0f2fd2ad82
lawshe <jessica@jlawshe.com>
2017-05-02 15:42:38 -0500

    Removed unused field

--- client/src/schema.js
+++ client/src/schema.js
@@ -1,8 +1,7 @@
 export const typeDefs = `
   type Channel {
     id: ID!                # "!" denotes a required field
-    name: String,
-    location: String
+    name: String
   }
   # This type specifies the entry points into our API. In this case
   # there is only one - "channels" - which returns a list of channels.
